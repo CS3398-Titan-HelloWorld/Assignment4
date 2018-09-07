1e288bb2876213ceebb2192f3b6c098eac14eab6
brk28 <brk28@txstate.edu>
2018-09-07 14:25:00 -0500

    Add files via upload
    Adding code to GitHub

---
+++ MainFunctionClass.java
@@ -0,0 +1,32 @@
+package src;
+
+public class ThreeSolidMain {
+	public static Manager tsManager = new Manager();
+
+	   // The entry main() method
+	   public static void main(String[] args)
+	   {
+
+	      try
+	      {
+	         System.out.format("Starting ... \n");
+	      }
+	      catch (Exception main_except)
+	      {
+	         main_except.printStackTrace();
+	      }
+
+	            try
+	      {
+	         System.out.format("Stopping ... \n");
+	      }
+	      catch (Exception main_except)
+	      {
+	         main_except.printStackTrace();
+	      }
+
+	      System.exit(0);
+
+	   }
+
+}
