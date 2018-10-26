ebe775d2fbe8acfe82f3b28dddba97676d423fde
RamseyD <42945168+RamseyD@users.noreply.github.com>
2018-10-26 14:35:31 -0500

    Update TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -4,15 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
  private Worker w = new Worker();
+ private SuperWorker SW = new SuperWorker();
  @Test
  @DisplayName("Work Method Test")
  public void testWorkMethod()
  {
  assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
  }
- private SuperWorker SW = new SuperWorker();
+  public void testEatMethod()
+ {
+ assertEquals(w.eat(),"I'm eating already!","Mismatch between test text and method text");
+ }
+ public void testSickMethod()
+ {
+ assertEquals(w.sick(),"I'm sick already!","Mismatch between test text and method text");
+ }
  
-  public void testWorkMethod(){
+ public void testWorkMethod(){
  	assertEquals(SW.work(), "Working hard. Just working hard.", "Mismatch between test text and method text");
  }
  public void testWorkMethod(){
