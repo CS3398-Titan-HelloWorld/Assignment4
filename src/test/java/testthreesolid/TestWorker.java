c420c6f6572dc02c62d4c9c7a691db167606efbd
nashbateman <33039086+nashbateman@users.noreply.github.com>
2018-10-26 14:17:14 -0500

    Update TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -11,10 +11,12 @@ public class TestWorker {
  {
  assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
  }
+  // The following test should pass becuase the string that the method returns is the same as the string being tested
  public void testEatMethod()
  {
  assertEquals(w.eat(),"Lunch time!","Mismatch between test text and method text");
  }
+  // The following test should fail becuase the string that the method returns is different than the string being tested
  public void testSickMethod()
  {
  assertEquals(w.sick(),"I'm sick already!","Mismatch between test text and method text");
