0e2f6c9cb8c2dbc3152bd08bc5eadf7d7f895469
iantex <icc6@txstate.edu>
2018-10-26 14:13:28 -0500

    Added tests for TempWorker
    DONT LOOK AT ME!!!!!!!!

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -10,5 +10,22 @@ public class TestWorker {
  	{
  		assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
  	}
+	
+	//This is the Section Ian implented
+	private TempWorker tW = new TempWorker();
+	@Test
+	@DisplayName("Testing Work Method in TempWorker")
+	public void testTempWork()
+	{
+		assertEquals(tW.work(),"Workin hard or hardly workin?", "Mismatch between test text and method text");
+		//This test should return true, as this is what TempWorker shoud say while working
+	}
+	@Test
+	@DisplayName("Testing Sick Method in TempWorker")
+	public void testTempSick()
+	{
+		assertEquals(tW.sick(),"I'm totally in the office, boss", "Mismatch between test text and method text");
+		//This should return false, because Temp Worker is a filthy liar
+	}
 }
 
