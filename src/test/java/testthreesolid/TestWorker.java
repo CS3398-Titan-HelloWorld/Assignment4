fe4359c431f87da2b9fbb72d8c045171bec4d28e
Bkelley3 <brk28@txstate.edu>
2018-10-26 15:06:36 -0500

    Added my passing and failing functions
    For RobotWorker

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -2,22 +2,34 @@ package src;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 public class TestWorker {
   
- private Worker w = new Worker();
- @Test
- @DisplayName("Work Method Test")
- public void testWorkMethod()
- {
- assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
- }
- public void testEatMethod()
- {
- assertEquals(w.eat(),"I'm eating already!","Mismatch between test text and method text");
- }
- public void testSickMethod()
- {
- assertEquals(w.sick(),"I'm sick already!","Mismatch between test text and method text");
- }
- 
+	private Worker w = new Worker();
+ 	@Test
+ 	@DisplayName("Work Method Test")
+ 	public void testWorkMethod()
+ 	{
+ 		assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+ 	}
+ 	public void testEatMethod()
+ 	{
+ 		assertEquals(w.eat(),"I'm eating already!","Mismatch between test text and method text");
+ 	}
+ 	public void testSickMethod()
+ 	{
+ 		assertEquals(w.sick(),"I'm sick already!","Mismatch between test text and method text");
+ 	}
+
+ 	private RobotWorker rw = new RobotWorker();
+ 	public void testRobotWorkerPass()
+ 	{
+ 		//Tests the work function in RobotWorker.java and should pass
+ 		assertEquals(rw.work(),"Beep Borp?", "Mismatch between test text and method text");
+ 	}
+ 	public void testRobotWorkerFail()
+ 	{
+ 		//Tests the reboot function in RobotWorker.java and should fail
+ 		assertEquals(rw.reboot(), "Take over the world", "Mismatch between test text and method text");
+ 	}
 }
