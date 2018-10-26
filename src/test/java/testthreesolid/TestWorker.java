cdaf72f1ba69720ac9711b92f68d806d7856a42e
RamseyD <42945168+RamseyD@users.noreply.github.com>
2018-10-26 14:31:48 -0500

    Test for SuperWorker

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -8,5 +8,14 @@ public class TestWorker {
  @DisplayName("Work Method Test")
  public void testWorkMethod()
  {
- assertEquals(w.work(),"I'm working already!",îMismatch between test text and method textî);
+ assertEquals(w.work(),"I'm working already!",‚ÄùMismatch between test text and method text‚Äù);
  }
+ private SuperWorker SW = new SuperWorker();
+ 
+  public void testWorkMethod(){
+ 	assertEquals(SW.work(), "Working hard. Just working hard.", "Mismatch between test text and method text");
+ }
+ public void testWorkMethod(){
+ 	assertEquals(SW.eat(), "Is there more coffee?", "Mismatch between test text and method text");
+ }
+}
