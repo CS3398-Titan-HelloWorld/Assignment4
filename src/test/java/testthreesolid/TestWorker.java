bebe8c3689b36173622def99881905dd83ac5108
nashbateman <33039086+nashbateman@users.noreply.github.com>
2018-10-26 13:39:13 -0500

    Update TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -10,4 +10,13 @@ public class TestWorker {
  {
  assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
  }
+ public void testEatMethod()
+ {
+ assertEquals(w.eat(),"I'm eating already!","Mismatch between test text and method text");
+ }
+ public void testSickMethod()
+ {
+ assertEquals(w.sick(),"I'm sick already!","Mismatch between test text and method text");
+ }
+ 
 }
