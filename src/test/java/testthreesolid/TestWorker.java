9316c1cc94d4280d17ead3aadf9b24832edff316
Bkelley3 <brk28@txstate.edu>
2018-10-26 13:05:44 -0500

    TestWorker Syntax
    There were missing brackets and semicolons

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,12 +1,14 @@
-package <src>;
+package src;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
- private Worker w = new Worker();
- @Test
- @DisplayName("Work Method Test")
- public void testWorkMethod()
- {
- assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
- }
+	private Worker w = new Worker();
+ 	@Test
+ 	@DisplayName("Work Method Test")
+ 	public void testWorkMethod()
+ 	{
+ 		assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+ 	}
+}
+
