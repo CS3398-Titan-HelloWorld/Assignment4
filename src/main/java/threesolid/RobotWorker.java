fab9fd29aa427d4b402312a3c87969f81f36b798
Bkelley3 <brk28@txstate.edu>
2018-10-26 15:12:00 -0500

    Changed the Void function to a String
    reboot()

--- src/main/java/threesolid/RobotWorker.java
+++ src/main/java/threesolid/RobotWorker.java
@@ -7,9 +7,10 @@ public class RobotWorker extends BaseWorker implements IReboot {
 		response = "Beep Borp?";
 		return response;
 	}
-	public void reboot() {
-		System.out.println("Rebooting");
-
+	public String reboot() {
+		String talking;
+		talking = "Rebooting";
+		return talking
 	}
 
 }
