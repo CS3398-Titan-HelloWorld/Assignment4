dc9f3bfb53eb4ca8750dcb69c19bc833788e59d9
RamseyD <42945168+RamseyD@users.noreply.github.com>
2018-10-26 14:45:19 -0500

    Try 3

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
  private Worker w = new Worker();
- private SuperWorker SW = new SuperWorker();
+ private SuperWorker sw = new SuperWorker();
  @Test
  @DisplayName("Work Method Test")
  public void testWorkMethod()
@@ -21,9 +21,9 @@ public class TestWorker {
  }
  
  public void testWorkMethod(){
- 	assertEquals(SW.work(), "Working hard. Just working hard.", "Mismatch between test text and method text");
+ 	assertEquals(sw.work(), "Working hard. Just working hard.", "Mismatch between test text and method text");
  }
  public void testWorkMethod(){
- 	assertEquals(SW.eat(), "Is there more coffee?", "Mismatch between test text and method text");
+ 	assertEquals(sw.eat(), "Is there more coffee?", "Mismatch between test text and method text");
  }
 }
